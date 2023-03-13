44865a00583552b853db9769372c7f8d5331226e
Carefree-happy <2507057523@qq.com>
2023-03-13 16:01:51 +0800

    feat: setState

--- src/setstate.js
+++ src/setstate.js
@@ -1,5 +1,5 @@
 import { Component } from "react"
-// setState只在合成事件和生命周期钩子函数中是异步的，而在原生事件中都是同步的
+// setState只在合成事件和生命周期钩子函数中是异步的，而在setTimeout或者原生dom事件中都是同步的
 class Test1 extends Component {
     state = {
         count: 0
@@ -62,3 +62,31 @@ class Test3 extends Component {
     }
 }
 // 0 1 2 2 如果只是回调函数，本身值不变
+
+class Test extends Component {
+    state = {
+        count: 0
+    }
+
+    componentDidMount() {
+        this.setState({count: this.state.count + 1})
+        console.log("setTimeout: " + this.state.count)
+        setTimeout(() => {
+            this.setState({count: this.state.count + 1})
+            console.log("setTimeout: " + this.state.count)
+        }, 0)
+    }
+
+    changeText() {
+        this.setState({
+          count: 4
+        }, () => {
+          console.log(this.state.count); // 你好啊,李银河
+        });
+    }
+
+    render() {
+        return <div onClick={() => this.changeText()}>{this.state.count}</div>
+    }
+}
+// setTimeout: 0 setTimeout: 2 4
