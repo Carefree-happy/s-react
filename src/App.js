94000bcb7dec40c9e4fa39607324906d0d262106
Carefree-happy <2507057523@qq.com>
2023-03-14 14:32:50 +0800

    fragment: returning multiple elements

--- src/App.js
+++ src/App.js
@@ -1,3 +1,23 @@
+import { Fragment } from "react";
+
 export default function App() {
-    return <p>Hello World!</p>;
+    return <Fragment>
+        <Post title="One" body="One body"/>
+        <Post title="two" body="Two body"/>
+    </Fragment>
+}
+
+function Post({ title, body}) {
+    return <>
+        <PostTitle title={title}/>
+        <PostBody body={body}/>
+    </>
+}
+
+function PostTitle({ title }) {
+    return <h1>{title}</h1>
+}
+
+function PostBody({ body }) {
+    return <article><p>{body}</p></article>
 }
