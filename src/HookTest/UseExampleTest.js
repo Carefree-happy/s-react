2079dabc79cceaf2ac2a59615f3c1d875a07df1b
Carefree-happy <2507057523@qq.com>
2023-03-15 13:32:20 +0800

    useEffect: 跟踪元素可见性

--- src/HookTest/UseExampleTest.js
+++ src/HookTest/UseExampleTest.js
@@ -4,29 +4,56 @@ export function UseExampleTest() {
     const [show, setShow] = useState(false);
     return (
         <>
-            <button onClick={() => setShow(!show)}>
-                {show ? "close" : "open"} dialog
-            </button>
-            <ModalDialog isOpen={show}>
-                Hello, Here!
-                <br/>
-                <button onClick={() => {setShow(false)}}>close dialog</button>
-            </ModalDialog>
+            <LongSection />
+            <Box />
+            <LongSection />
+            <Box />
+            <LongSection />
         </>
     );
 }
 
-function ModalDialog({ isOpen, children }) {
-    const ref = useRef();
+function LongSection() {
+    const items = [];
+    for (let i = 0; i < 50; i++) {
+        items.push(<li key={i}>Item #{i} (keep scrolling)</li>);
+    }
+    return <ul>{items}</ul>;
+}
+
+function Box() {
+    const ref = useRef(null);
 
     useEffect(() => {
-        if(!isOpen) return;
-        const dialog = ref.current;
-        dialog.showModal();
+        const div = ref.current;
+        const observer = new IntersectionObserver((entries) => {
+            const entry = entries[0];
+            if (entry.isIntersecting) {
+                document.body.style.backgroundColor = "black";
+                document.body.style.color = "white";
+            } else {
+                document.body.style.backgroundColor = "white";
+                document.body.style.color = "black";
+            }
+        });
+        observer.observe(div, {
+            threshold: 1.0,
+        });
         return () => {
-            dialog.close();
+            observer.disconnect();
         };
-    }, [isOpen])
+    }, []);
 
-    return <dialog ref={ref}>{children}</dialog>
+    return (
+        <div
+            ref={ref}
+            style={{
+                margin: 20,
+                height: 100,
+                width: 100,
+                border: "2px solid black",
+                backgroundColor: "blue",
+            }}
+        />
+    );
 }
