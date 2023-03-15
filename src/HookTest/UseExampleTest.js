8a3da87e7cc40837461939df520065ebbea5bb12
Carefree-happy <2507057523@qq.com>
2023-03-15 15:20:19 +0800

    feat: click counter ref

--- src/HookTest/UseExampleTest.js
+++ src/HookTest/UseExampleTest.js
@@ -1,112 +1,14 @@
 import { useEffect, useLayoutEffect, useRef, useState } from "react";
-import { createPortal } from 'react-dom';
+import { createPortal } from "react-dom";
 
 export function UseExampleTest() {
-    return (
-        <div>
-            <ButtonWithTooltip
-                tooltipContent={
-                    <div>
-                        This tooltip does not fit above the button.
-                        <br />
-                        This is why it's displayed below instead!
-                    </div>
-                }
-            >
-                Hover over me (tooltip above)
-            </ButtonWithTooltip>
-            <div style={{ height: 50 }} />
-            <ButtonWithTooltip
-                tooltipContent={<div>This tooltip fits above the button</div>}
-            >
-                Hover over me (tooltip below)
-            </ButtonWithTooltip>
-            <div style={{ height: 50 }} />
-            <ButtonWithTooltip
-                tooltipContent={<div>This tooltip fits above the button</div>}
-            >
-                Hover over me (tooltip below)
-            </ButtonWithTooltip>
-        </div>
-    );
-}
+    let ref = useRef(0);
 
-function ButtonWithTooltip({ tooltipContent, ...rest }) {
-    const [targetRect, setTargetRect] = useState(null);
-    const buttonRef = useRef(null);
-    return (
-        <>
-            <button
-                {...rest}
-                ref={buttonRef}
-                onPointerEnter={() => {
-                    const rect = buttonRef.current.getBoundingClientRect();
-                    setTargetRect({
-                        left: rect.left,
-                        top: rect.top,
-                        right: rect.right,
-                        bottom: rect.bottom,
-                    });
-                }}
-                onPointerLeave={() => {
-                    setTargetRect(null);
-                }}
-            />
-            {targetRect !== null && (
-                <Tooltip targetRect={targetRect}>{tooltipContent}</Tooltip>
-            )}
-        </>
-    );
-}
-
-function Tooltip({ children, targetRect }) {
-    const ref = useRef(null);
-    const [tooltipHeight, setTooltipHeight] = useState(0);
-
-    let now = performance.now();
-    while (performance.now() - now < 500) {
-      // Do nothing for a bit...
-    }
-    // 仅仅替换 useEffect 或者 useLayoutEffect
-    useLayoutEffect(() => {
-        const { height } = ref.current.getBoundingClientRect();
-        setTooltipHeight(height);
-        console.log("Measured tooltip height: " + height);
-    }, []);
-
-    let tooltipX = 0;
-    let tooltipY = 0;
-    if (targetRect !== null) {
-        tooltipX = targetRect.left;
-        tooltipY = targetRect.top - tooltipHeight;
-        if (tooltipY < 0) {
-            // It doesn't fit above, so place below.
-            tooltipY = targetRect.bottom;
-        }
+    function handleClick() {
+        // 此处证明ref.current 中的值跟渲染无关
+        ref.current = ref.current + 1;
+        alert("You clicked " + ref.current + " times!");
     }
 
-    return createPortal(
-        <TooltipContainer x={tooltipX} y={tooltipY} contentRef={ref}>
-            {children}
-        </TooltipContainer>,
-        document.body
-    );
-}
-
-function TooltipContainer({ children, x, y, contentRef }) {
-    return (
-        <div
-            style={{
-                position: "absolute",
-                pointerEvents: "none",
-                left: 0,
-                top: 0,
-                transform: `translate3d(${x}px, ${y}px, 0)`,
-            }}
-        >
-            <div ref={contentRef} className="tooltip">
-                {children}
-            </div>
-        </div>
-    );
+    return <button onClick={handleClick}>Click me! {ref.current}</button>;
 }
