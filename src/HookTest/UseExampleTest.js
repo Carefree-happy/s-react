53a5ae06f0495a4f3ce3aad4a50bb86155c96272
Carefree-happy <2507057523@qq.com>
2023-03-15 15:34:12 +0800

    useRef: Focusing a text input

--- src/HookTest/UseExampleTest.js
+++ src/HookTest/UseExampleTest.js
@@ -1,34 +1,16 @@
-import { useState, useRef } from 'react';
+import { useRef } from "react";
 
 export function UseExampleTest() {
-    const [startTime, setStartTime] = useState(null);
-    const [now, setNow] = useState(null);
-    const intervalRef = useRef(null);
+    const inputRef = useRef(null);
 
-    function handleStart() {
-        setStartTime(Date.now());
-        setNow(Date.now());
-
-        clearInterval(intervalRef.current);
-        intervalRef.current = setInterval(() => {
-            setNow(Date.now());
-        }, 10);
-    }
-
-    function handleStop() {
-        clearInterval(intervalRef.current);
-    }
-
-    let secondsPassed = 0;
-    if (startTime != null && now != null) {
-        secondsPassed = (now - startTime) / 100000;
+    function handleClick() {
+        inputRef.current.focus();
     }
 
     return (
         <>
-            <h1>Time passed: {secondsPassed.toFixed(3)}</h1>
-            <button onClick={handleStart}>Start</button>
-            <button onClick={handleStop}>Stop</button>
+            <input ref={inputRef} />
+            <button onClick={handleClick}>Focus the input</button>
         </>
     );
 }
