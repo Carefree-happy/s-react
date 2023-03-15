6c1476bb854923eade81deb17b079f97711ad487
Carefree-happy <2507057523@qq.com>
2023-03-15 16:24:48 +0800

    useImperativeHandle: Exposing your own imperative methods

--- src/HookTest/UseExampleTest.js
+++ src/HookTest/UseExampleTest.js
@@ -1,32 +1,86 @@
-import { forwardRef, useImperativeHandle, useRef } from "react";
+import { forwardRef, useImperativeHandle, useRef, useState } from "react";
 
 export function UseExampleTest() {
-    const inputRef = useRef(null);
+    const postRef = useRef(null);
 
     function handleClick() {
-        inputRef.current.focus();
+        postRef.current.scrollAndFocusAddComment();
     }
 
     return (
         <>
-            <MyInput ref={inputRef} />
-            <button onClick={handleClick}>Focus the input</button>
+            <button onClick={handleClick}>Write a comment</button>
+            <Post ref={postRef} />
         </>
     );
 }
 
-const MyInput = forwardRef((props, ref) => {
-    const inputRef = useRef(null)
+const Post = forwardRef((props, ref) => {
+    const commentsRef = useRef(null);
+    const addCommentRef = useRef(null);
 
-    useImperativeHandle(ref, () => {
-        return {
-            focus() {
-                inputRef.current.focus();
-            },
-            scrollIntoView() {
-                inputRef.current.scrollIntoView();
-            }
-        }
-    })
-    return <input {...props} ref={ref} />;
+    useImperativeHandle(
+        ref,
+        () => {
+            return {
+                scrollAndFocusAddComment() {
+                    commentsRef.current.scrollToBottom();
+                    addCommentRef.current.focus();
+                },
+            };
+        },
+        []
+    );
+
+    return (
+        <>
+            <article>
+                <p>Welcome to my blog!</p>
+            </article>
+            <CommentList ref={commentsRef} />
+            <AddComment ref={addCommentRef} />
+        </>
+    );
+});
+
+const CommentList = forwardRef(function CommentList(props, ref) {
+    const divRef = useRef(null);
+
+    useImperativeHandle(
+        ref,
+        () => {
+            return {
+                scrollToBottom() {
+                    const node = divRef.current;
+                    node.scrollTop = node.scrollHeight;
+                },
+            };
+        },
+        []
+    );
+
+    let comments = [];
+    for (let i = 0; i < 50; i++) {
+        comments.push(<p key={i}>Comment #{i}</p>);
+    }
+
+    return (
+        <div
+            className="CommentList"
+            ref={divRef}
+            style={{
+                height: 100,
+                overflow: "scroll",
+                border: "1px solid black",
+                marginTop: 20,
+                marginBottom: 20,
+            }}
+        >
+            {comments}
+        </div>
+    );
+});
+
+const AddComment = forwardRef(function AddComment(props, ref) {
+    return <input placeholder="Add comment..." ref={ref} />;
 });
