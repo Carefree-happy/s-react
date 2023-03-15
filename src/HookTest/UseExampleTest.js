57eef0dff2b7ffb2e65764938ebeff94cabf8a0f
Carefree-happy <2507057523@qq.com>
2023-03-15 14:46:35 +0800

    useEffect: Updating state based on previous state from an Effect

--- src/HookTest/UseExampleTest.js
+++ src/HookTest/UseExampleTest.js
@@ -1,70 +1,14 @@
 import { useEffect, useRef, useState } from "react";
 
 export function UseExampleTest() {
-    const [show, setShow] = useState(false);
-    return (
-        <>
-            <LongSection />
-            <Box />
-            <LongSection />
-            <Box />
-            <LongSection />
-        </>
-    );
-}
+    const [count, setCount] = useState(0);
 
-function LongSection() {
-    const items = [];
-    for (let i = 0; i < 50; i++) {
-        items.push(<li key={i}>Item #{i} (keep scrolling)</li>);
-    }
-    return <ul>{items}</ul>;
-}
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCount(c => c + 1); // ✅ Pass a state updater
+    }, 1000);
+    return () => clearInterval(intervalId);
+  }, []); // ✅ Now count is not a dependency
 
-function Box() {
-    const ref = useRef(null);
-    const isIntersecting = useIntersectionObserver(ref);
-
-    useEffect(() => {
-        if (isIntersecting) {
-            document.body.style.backgroundColor = "black";
-            document.body.style.color = "white";
-        } else {
-            document.body.style.backgroundColor = "white";
-            document.body.style.color = "black";
-        }
-    }, [isIntersecting]);
-
-    return (
-        <div
-            ref={ref}
-            style={{
-                margin: 20,
-                height: 100,
-                width: 100,
-                border: "2px solid black",
-                backgroundColor: "blue",
-            }}
-        />
-    );
-}
-
-function useIntersectionObserver(ref) {
-    const [isIntersecting, setIsIntersecting] = useState(false);
-
-    useEffect(() => {
-        const div = ref.current;
-        const observer = new IntersectionObserver((entries) => {
-            const entry = entries[0];
-            setIsIntersecting(entry.isIntersecting);
-        });
-        observer.observe(div, {
-            threshold: 1.0,
-        });
-        return () => {
-            observer.disconnect();
-        };
-    }, [ref]);
-
-    return isIntersecting;
+  return <h1>{count}</h1>;
 }
