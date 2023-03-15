177285257e3dce88f5adb71e2e889df0c16743ca
Carefree-happy <2507057523@qq.com>
2023-03-15 14:22:55 +0800

    useEffect: 自定义useWindowListener hook

--- src/HookTest/UseExampleTest.js
+++ src/HookTest/UseExampleTest.js
@@ -1,73 +1,34 @@
 import { useEffect, useState } from "react";
 
 export function UseExampleTest() {
-    const [roomId, setRoomId] = useState("general");
-    const [show, setShow] = useState(false);
-    return (
-        <>
-            <label>
-                Choose the chat room:{" "}
-                <select
-                    value={roomId}
-                    onChange={(e) => setRoomId(e.target.value)}
-                >
-                    <option value="general">general</option>
-                    <option value="travel">travel</option>
-                    <option value="music">music</option>
-                </select>
-            </label>
-            <button onClick={() => setShow(!show)}>
-                {show ? "Close chat" : "Open chat"}
-            </button>
-            {show && <hr />}
-            {show && <ChatRoom roomId={roomId} />}
-        </>
-    );
-}
+    const [position, setPosition] = useState({ x: 0, y: 0 });
 
-function ChatRoom({ roomId }) {
-    const [serverUrl, setServerUrl] = useState("https://localhost:1234");
-
-    useChatRoom({
-        roomId: roomId,
-        serverUrl: serverUrl,
+    useWindowListener("pointermove", (e) => {
+        setPosition({ x: e.clientX, y: e.clientY });
     });
 
     return (
-        <>
-            <label>
-                Server URL:{" "}
-                <input
-                    value={serverUrl}
-                    onChange={(e) => setServerUrl(e.target.value)}
-                />
-            </label>
-            <h1>Welcome to the {roomId} room!</h1>
-        </>
+        <div
+            style={{
+                position: "absolute",
+                backgroundColor: "pink",
+                opacity: 0.6,
+                borderRadius: "50%",
+                transform: `translate(${position.x}px, ${position.y}px)`,
+                left: -20,
+                top: -20,
+                width: 40,
+                height: 40,
+            }}
+        />
     );
 }
 
-function useChatRoom({ roomId, serverUrl }) {
+function useWindowListener(eventType, listener) {
     useEffect(() => {
-        const connection = createConnection(serverUrl, roomId);
-        connection.connect();
+        window.addEventListener(eventType, listener);
         return () => {
-            connection.disconnect();
+            window.removeEventListener(eventType, listener);
         };
-    }, [roomId, serverUrl]);
-}
-
-function createConnection(serverUrl, roomId) {
-    return {
-        connect() {
-            console.log(
-                '✅ Connecting to "' + roomId + '" room at ' + serverUrl + "..."
-            );
-        },
-        disconnect() {
-            console.log(
-                '❌ Disconnected from "' + roomId + '" room at ' + serverUrl
-            );
-        },
-    };
+    }, [eventType, listener]);
 }
