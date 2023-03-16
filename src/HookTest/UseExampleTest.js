054fe36fff6a5030292765ac6d4b2209264e45b3
Carefree-happy <2507057523@qq.com>
2023-03-17 00:44:27 +0800

    forward: Playing and pausing a video

--- src/HookTest/UseExampleTest.js
+++ src/HookTest/UseExampleTest.js
@@ -1,30 +1,26 @@
 import { forwardRef, useRef } from "react";
 
-const todos = createTodos();
-
 export function UseExampleTest() {
     const ref = useRef(null);
-
-    function handleClick() {
-        ref.current.focus();
-    }
-
     return (
-        <form>
-            <MyInput label="Enter your name:" ref={ref} />
-            <button type="button" onClick={handleClick}>
-                Edit
-            </button>
-        </form>
+        <>
+            <button onClick={() => ref.current.play()}>Play</button>
+            <button onClick={() => ref.current.pause()}>Pause</button>
+            <br />
+            <MyVideoPlayer
+                ref={ref}
+                src="https://interactive-examples.mdn.mozilla.net/media/cc0-videos/flower.mp4"
+                type="video/mp4"
+                width="250"
+            />
+        </>
     );
 }
 
-const MyInput = forwardRef(function MyInput(props, ref) {
-    const { label, ...otherProps } = props;
+const MyVideoPlayer = forwardRef(function VideoPlayer({ src, type, width }, ref) {
     return (
-        <label>
-            {label}
-            <input {...otherProps} ref={ref} />
-        </label>
+        <video width={width} ref={ref}>
+            <source src={src} type={type} />
+        </video>
     );
 });
