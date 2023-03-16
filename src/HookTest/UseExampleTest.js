eedd6291ad00e186170fc07fbe04ed97bb9524b9
Carefree-happy <2507057523@qq.com>
2023-03-17 00:46:42 +0800

    forward: exposing an imperative handle instead of A DOM node

--- src/HookTest/UseExampleTest.js
+++ src/HookTest/UseExampleTest.js
@@ -1,26 +1,43 @@
-import { forwardRef, useRef } from "react";
+import { forwardRef, useRef, useState } from "react";
 
 export function UseExampleTest() {
     const ref = useRef(null);
+
+    function handleClick() {
+        ref.current.focus();
+    }
+
     return (
-        <>
-            <button onClick={() => ref.current.play()}>Play</button>
-            <button onClick={() => ref.current.pause()}>Pause</button>
-            <br />
-            <MyVideoPlayer
-                ref={ref}
-                src="https://interactive-examples.mdn.mozilla.net/media/cc0-videos/flower.mp4"
-                type="video/mp4"
-                width="250"
-            />
-        </>
+        <form>
+            <FormField label="Enter your name:" ref={ref} isRequired={true} />
+            <button type="button" onClick={handleClick}>
+                Edit
+            </button>
+        </form>
     );
 }
 
-const MyVideoPlayer = forwardRef(function VideoPlayer({ src, type, width }, ref) {
+const FormField = forwardRef(function FormField({ label, isRequired }, ref) {
+    const [value, setValue] = useState("");
     return (
-        <video width={width} ref={ref}>
-            <source src={src} type={type} />
-        </video>
+        <>
+            <MyInput
+                ref={ref}
+                label={label}
+                value={value}
+                onChange={(e) => setValue(e.target.value)}
+            />
+            {isRequired && value === "" && <i>Required</i>}
+        </>
+    );
+});
+
+const MyInput = forwardRef((props, ref) => {
+    const { label, ...otherProps } = props;
+    return (
+        <label>
+            {label}
+            <input {...otherProps} ref={ref} />
+        </label>
     );
 });
